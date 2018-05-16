7d6b446a881f579e02667301ddbb7d57511fd83c
Admin <Ahmed@admins-mac.local>
2018-05-16 17:21:13 +0300

    Initial Commit

---
+++ copyCat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  copyCat
+//
+//  Created by Ahmed on 9/1/1439 AH.
+//  Copyright © 1439 AH Steph_saber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
